7a610fd3743f42c180afb0978dcca59b0f426e1a
Arie <arieinjector@gmail.com>
2023-03-24 09:42:20 +0700

    add title & meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,23 @@ import Main from "@/components/Main";
 import Nav from "@/components/Nav";
 import Projects from "@/components/Projects";
 import Testimoni from "@/components/Testimoni";
+import Head from "next/head";
 
 export default function Home () {
   return (
     <div>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Jhontraktor is a startup construction field" />
+        <meta name="keywords" content="Keywords" />
+        <title>Jhontraktor</title>
+        <meta name="theme-color" content="#FF7420" />
+      </Head>
       <Nav />
       <Main />
       <Experience />
